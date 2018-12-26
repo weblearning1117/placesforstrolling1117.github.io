783f2dc46ea7dc3c9a6a15e0d3cad8ba1eb34130
placesforstrolling1117 <45429502+placesforstrolling1117@users.noreply.github.com>
2018-12-26 16:52:07 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,373 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>PLACES FOR STROLLING</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+  <link rel="stylesheet" href="css/style3.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,700" rel="stylesheet">
+</head>
+
+<body>
+  <p class="next active disabled first second sect-3 click"><span class="txt">Next</span> <i class="fa fa-long-arrow-right"
+      aria-hidden="true"></i>
+  </p>
+  <div class="warning">
+    <p>Please answer all questions <span class="exclamation">!</span></p>
+  </div>
+  <p class="main-back click"><span class="main-back-text">Back</span><i class="fa fa-undo" aria-hidden="true"></i>
+  </p>
+  <div class="header">
+    <ul class="center">
+      <li>
+        <span class="main">places for strolling</span>
+      </li>
+    </ul>
+    <ul class="right">
+      <li>
+        <a href="indexRu.html" class="ru language">RU/</a>
+        <a href="#" class="en language">EN/</a>
+        <a href="indexUz.html" class="uz language">UZ</a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="container section-1">
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2> What is Your campaign?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question2 click" data-question="2" data-many="4">
+      <div class="col-lg-2 col-3 answer answer1" data-campaign="friends" data-order="1">
+        <p> Friends</p>
+        <i class="fa fa-male" aria-hidden="true"></i>
+        <i class="fa fa-male" aria-hidden="true"></i>
+        <i class="fa fa-female" aria-hidden="true"></i>
+        <i class="fa fa-female" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-campaign="family" data-order="2">
+        <p> Family</p>
+        <i class="fa fa-male" aria-hidden="true"></i>
+        <i class="fa fa-child" aria-hidden="true"></i>
+        <i class="fa fa-female" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-campaign="single" data-order="3">
+        <p> Single</p>
+        <i class="fa fa-male" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer4" data-campaign="pare" data-order="4">
+        <p> Couple</p>
+        <i class="fa fa-male" aria-hidden="true"></i>
+        <i class="fa fa-female" aria-hidden="true"></i>
+
+      </div>
+      <div class="back back2"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2> How many people in Your campaign?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question3 click" data-i="3" data-many="3">
+      <div class="col-lg-2 col-3 answer answer1" data-amount="1-5" data-order="1">
+        <p> 1-5 people</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-amount="5-10" data-order="2">
+        <p> 5-10 people</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-amount="10-15" data-order="3">
+        <p> 10-15 people</p>
+      </div>
+      <div class="back back3"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="row ">
+      <div class="col-lg-12 question">
+        <h2> What time do You prefer to stroll?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question4 click" data-i="4" data-many="3">
+      <div class="col-lg-2 col-3 answer answer1" data-time="morning" data-order="1">
+        <p> Morning</p>
+        <i class="fa fa-coffee" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-time="daytime" data-order="2">
+        <p> Daytime</p>
+        <i class="fa fa-sun-o" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-time="evening" data-order="3">
+        <p> Evening</p>
+        <i class="fa fa-moon-o" aria-hidden="true"></i>
+      </div>
+      <div class="back back4"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2> How old are You?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question1 click" data-i="1" data-many="4">
+      <div class="col-lg-2 col-3 answer answer1" data-age="10-16" data-order="1">
+        <p> 10-16 years old</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-age="17-25" data-order="2">
+        <p> 17-25 years old</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-age="26-38" data-order="3">
+        <p> 26-38 years old</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer4" data-age="39+" data-order="4">
+        <p> <span class="margin-right">39+</span> years old</p>
+      </div>
+      <div class="back back1"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <div class="container section-2 active">
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2> How would you like to spend Your time?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question5 click" data-many="3">
+      <div class="col-lg-2 col-3 answer answer1" data-doing="eat" data-order="1">
+        <p>Eating </p>
+        <i class="fa fa-cutlery" aria-hidden="true"></i>
+      </div>
+
+      <div class="col-lg-2 col-3 answer answer2" data-doing="walk" data-order="2">
+        <p>Walking</p>
+        <i class="fa fa-blind" aria-hidden="true"></i>
+
+      </div>
+
+      <div class="col-lg-2 col-3 answer answer3" data-doing="pleasure" data-order="3">
+        <p>Pleasure</p>
+        <i class="fa fa-gamepad" aria-hidden="true"></i>
+
+      </div>
+
+      <div class="back back5"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+
+    </div>
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2>In what district do You want to go for a walk?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question6 click" data-many="3" data-row="1">
+      <div class="col-lg-2 col-3 answer answer1" data-order="1" data-district="bektemir">
+        <p class="reltop">Bektemir </p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-order="2" data-district="mirzo-ulugbek">
+        <p>Mirzo-Ulugbek </p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-order="3" data-district="mirabad">
+        <p class="reltop">Mirabad </p>
+      </div>
+      <div class="back back6"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="row justify-content-center question6 question7 click" data-many="3" data-row="2">
+      <div class="col-lg-2 col-3 answer answer4" data-order="1" data-district="sergeli">
+        <p class="reltop">Sergeli </p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer5" data-order="2" data-district="uchtepa">
+        <p class="reltop"> Uchtepa </p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer6" data-order="3" data-district="hamza">
+        <p class="reltop"> Hamza </p>
+      </div>
+    </div>
+    <div class="row justify-content-center question6 question8 click" data-many="3" data-row="3">
+      <div class="col-lg-2 col-3 answer answer7" data-order="1" data-district="shayhantahur">
+        <p class="reltop relright">Shayhantahur</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer8" data-order="2" data-district="yunusabad">
+        <p class="reltop">Yunusabad</p>
+        <i class="fa fa-arrows-v" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer9" data-order="3" data-district="chilanzar">
+        <p class="reltop">Chilanzar </p>
+      </div>
+    </div>
+    <div class="row justify-content-center question6 question9 click" data-many="2" data-row="4">
+      <div class="col-lg-2 col-3 answer answer10" data-order="1" data-district="sabir-rakhimov">
+        <p> Sabir-Rakhimov </p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer11" data-order="2" data-district="yakkasaray">
+        <p class="reltop relrightyak">Yakkasaray </p>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+  <div class="section-3 active first">
+    <div class="container">
+      <div class="row justify-content-center click main-place" data-i="1" data-many="3">
+          <div class="place-extra-1 extra">
+            </div> 
+            <div class="place-extra-2 extra">
+            </div> 
+        <div class="col-lg-4 col-6 places place1" data-order="1">
+          <div class="place" data-extra-1="" data-extra-2="">
+          </div>
+         
+          <h4 class="title">Botanical Garden</h4>
+          <p class="description">
+            Bogishamol street, 232 (Yunus-abad district) <br>
+            Landmark: Tashkent Zoo <br>
+            Phone number: (+998) 71-289-1052, (+998) 71-289-1052 <br>
+            Tashkent Botanical Garden is the second largest in the CIS. In 2011, the garden was visited by about 65
+            thousand people.
+            The garden's collection is estimated at 6,000 species, forms and varieties of plants (mainly from the
+            temperate zone), the number of species assessed in 2013 is 4,500. According to the Tashkent encyclopedia
+            (1983), trees and shrubs account for 2,500 species (this figure reproduced in the "National Encyclopedia of
+            Uzbekistan"), including 170 conifers, 2200 species of one, two- and perennial herbs
+          </p>
+          <p class="description-small">Landmark: Tashkent Zoo <br>
+            Phone number: (+998) 71-289-1052, (+998) 71-289-1052 <br>
+            Work-time:545464
+          </p>
+        </div>
+        <div class="col-lg-4 col-6 places place2" data-order="2">
+          <div class="place">
+          </div>
+          <h4 class="title">Japanese garden</h4>
+          <p class="description">
+            Amir Temur street, 107 (Yunus-abad district) <br>
+            Landmark: "INTERNATIONAL HOTEL TASHKENT" <br>
+            Phone number: (+998) 71-238-5626 <br>
+            Work-time:10:00-19:00. <br>
+            Price for entrance to the Japanese Garden in Tashkent
+            Ticket for an adult - 20,000 soums. Ticket for pensioners - 10,000 soums. <br>
+            Japanese Garden in Tashkent - a garden in the center of the capital, which has become one of the most
+            magical sights
+            cities. A peculiar Japanese island in the middle of a big city! Gazebo, tea house, lake, cozy
+            wooden bridges, the diversity of birds - all this successfully conveys Japanese flavor. I want to come here
+            meditate or walk, being surrounded by majestic greenery and peaceful birds.</p>
+          <p class="description-small"> Landmark: "INTERNATIONAL HOTEL TASHKENT" <br>
+            Phone number: (+998) 71-238-5626 <br>
+            Work-time: 10:00-19:00.</p>
+        </div>
+        <div class="col-lg-4 col-6 places place3" data-order="3">
+          <div class="place">
+          </div>
+          <h4 class="title">Park3</h4>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. At doloribus possimus ab
+            debitis ullam nihil? Exercitationem rem laborum obcaecati dolorum possimus optio reprehenderit fuga cum,
+            alias, nesciunt fugiat fugit, cumque quidem esse praesentium eius omnis! Eveniet iure consequatur aliquid
+            recusandae fugit excepturi sit doloremque mollitia iusto atque illum facere tenetur necessitatibus quidem
+            accusamus, incidunt voluptates autem, ratione hic, aut numquam iste. Maxime laboriosam fugiat totam
+            cupiditate praesentium dolorum eius aut modi, nihil aperiam, vero quae?</p>
+          <p class="description-small">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi laborum rem
+            minus dignissimos libero enim voluptatem nesciunt, quo nulla modi...</p>
+        </div>
+
+        <div class="col-lg-4 col-6 places place4" data-order="4">
+          <div class="place">
+          </div>
+          <h4 class="title">Park4</h4>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. At doloribus possimus ab
+            debitis ullam nihil? Exercitationem rem laborum obcaecati dolorum possimus optio reprehenderit fuga cum,
+            alias, nesciunt fugiat fugit, cumque quidem esse praesentium eius omnis! Eveniet iure consequatur aliquid
+            recusandae fugit excepturi sit doloremque mollitia iusto atque illum facere tenetur necessitatibus quidem
+            accusamus, incidunt voluptates autem, ratione hic, aut numquam iste. Maxime laboriosam fugiat totam
+            cupiditate praesentium dolorum eius aut modi, nihil aperiam, vero quae?</p>
+          <p class="description-small">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi laborum rem
+            minus dignissimos libero enim voluptatem nesciunt, quo nulla modi...</p>
+        </div>
+        <div class="col-lg-4 col-6 places place5" data-order="5">
+          <div class="place">
+          </div>
+          <h4 class="title">Park5</h4>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. At doloribus possimus ab
+            debitis ullam nihil? Exercitationem rem laborum obcaecati dolorum possimus optio reprehenderit fuga cum,
+            alias, nesciunt fugiat fugit, cumque quidem esse praesentium eius omnis! Eveniet iure consequatur aliquid
+            recusandae fugit excepturi sit doloremque mollitia iusto atque illum facere tenetur necessitatibus quidem
+            accusamus, incidunt voluptates autem, ratione hic, aut numquam iste. Maxime laboriosam fugiat totam
+            cupiditate praesentium dolorum eius aut modi, nihil aperiam, vero quae?</p>
+          <p class="description-small">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi laborum rem
+            minus dignissimos libero enim voluptatem nesciunt, quo nulla modi...</p>
+        </div>
+        <div class="col-lg-4 col-6 places place6" data-order="6">
+          <div class="place">
+          </div>
+          <h4 class="title">Park6</h4>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. At doloribus possimus ab
+            debitis ullam nihil? Exercitationem rem laborum obcaecati dolorum possimus optio reprehenderit fuga cum,
+            alias, nesciunt fugiat fugit, cumque quidem esse praesentium eius omnis! Eveniet iure consequatur aliquid
+            recusandae fugit excepturi sit doloremque mollitia iusto atque illum facere tenetur necessitatibus quidem
+            accusamus, incidunt voluptates autem, ratione hic, aut numquam iste. Maxime laboriosam fugiat totam
+            cupiditate praesentium dolorum eius aut modi, nihil aperiam, vero quae?</p>
+          <p class="description-small">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi laborum rem
+            minus dignissimos libero enim voluptatem nesciunt, quo nulla modi...</p>
+        </div>
+        <div class="back back7"> <i class="fa fa-undo" aria-hidden="true"></i>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <div class="footer">
+    <p>&copy; Copyright</p>
+  </div>
+
+  <script src="js/jquery_3.2.1.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ indexRu.php
@@ -0,0 +1,233 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>PLACES FOR STROLLING</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+  <link rel="stylesheet" href="css/styleRu.css">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,700" rel="stylesheet"> 
+</head>
+
+<body>
+  <div class="warning"><p>Пожалуйста, ответьте на все вопросы <span class="exclamation">!</span></p> </div>
+
+  <div class="header">
+    <ul class="center">
+      <li>
+        <span class="main">places for strolling</span>
+      </li>
+    </ul>
+    <ul class="right">
+      <li>
+        <a href="#" class="ru language">RU/</a>
+        <a href="index.html" class="en language">EN/</a>
+        <a href="indexUz.html" class="uz language">UZ</a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="container section-1">
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2>Какая у Вас компания?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question2 click" data-question="2" data-many="4">
+      <div class="col-lg-2 col-3 answer answer1" data-campaign="friends" data-order="1">
+        <p>Друзья</p>
+        <i class="fa fa-male" aria-hidden="true"></i>
+        <i class="fa fa-male" aria-hidden="true"></i>
+        <i class="fa fa-female" aria-hidden="true"></i>
+        <i class="fa fa-female" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-campaign="family" data-order="2">
+        <p>Семья</p>
+        <i class="fa fa-male" aria-hidden="true"></i>
+        <i class="fa fa-child" aria-hidden="true"></i>
+        <i class="fa fa-female" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-campaign="single" data-order="3">
+        <p>Один</p>
+        <i class="fa fa-male" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer4" data-campaign="pare" data-order="4">
+        <p>Пара</p>
+        <i class="fa fa-male" aria-hidden="true"></i>
+        <i class="fa fa-female" aria-hidden="true"></i>
+
+      </div>
+      <div class="back back2"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2>Сколько людей в Вашей компании?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question3 click" data-i ="3" data-many="3">
+      <div class="col-lg-2 col-3 answer answer1" data-amount="1-5" data-order="1">
+        <p>1-5 человек</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-amount="5-10" data-order="2">
+        <p>5-10 человек</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-amount="10-15" data-order="3">
+        <p>10-15 человек</p>
+      </div>
+      <div class="back back3"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="row ">
+      <div class="col-lg-12 question">
+        <h2>Какое время Вы предпочитаете для прогулки?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question4 click" data-i ="4" data-many="3">
+      <div class="col-lg-2 col-3 answer answer1" data-time="morning" data-order="1">
+        <p>Утро</p>
+        <i class="fa fa-coffee" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-time="daytime" data-order="2">
+        <p>День</p>
+        <i class="fa fa-sun-o" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-time="evening" data-order="3">
+        <p>Вечер</p>
+        <i class="fa fa-moon-o" aria-hidden="true"></i>
+      </div>
+      <div class="back back4"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2>Сколько Вам лет?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question1 click" data-i ="1" data-many="4">
+      <div class="col-lg-2 col-3 answer answer1" data-age="10-16" data-order="1">
+        <p>10-16<br> лет</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-age="17-25" data-order="2">
+        <p>17-25<br> лет</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3" data-age="26-38" data-order="3">
+        <p>26-38<br> лет</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer4" data-age="39+"  data-order="4">
+        <p> <span class="margin-right">39+</span><br> лет</p>
+      </div>
+      <div class="back back1"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <p class="next active disabled first"><span class="txt">Вперед</span>  <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+    </p>
+   
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <div class="container section-2 active">
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2>Как Вы продпочитаете провести Ваше время?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question5 click" data-many ="3">
+      <div class="col-lg-2 col-3 answer answer1" data-doing = "eat" data-order="1">
+        <p>Поесть </p>
+        <i class="fa fa-cutlery" aria-hidden="true"></i>
+      </div>
+
+      <div class="col-lg-2 col-3 answer answer2" data-doing = "walk" data-order="2">
+        <p>Погулять</p>
+        <i class="fa fa-blind" aria-hidden="true"></i>
+
+      </div>
+
+      <div class="col-lg-2 col-3 answer answer3" data-doing = "pleasure" data-order="3">
+        <p>Развлечься</p>
+        <i class="fa fa-gamepad" aria-hidden="true"></i>
+
+      </div>
+
+      <div class="back back5"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+
+    </div>
+    <div class="row">
+      <div class="col-lg-12 question">
+        <h2>В каком районе Вы бы хотели погулять?</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center question6 click" data-many="3" data-row= "1">
+      <div class="col-lg-2 col-3 answer answer1" data-order="1">
+        <p class="reltop">Бектимир</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer2" data-order="2">
+        <p>Мирзо-Улугбек</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer3"  data-order="3">
+        <p class="reltop">Мирабад</p>
+      </div>
+      <div class="back back6"> <i class="fa fa-undo" aria-hidden="true"></i>
+      </div>
+    </div>
+    <div class="row justify-content-center question6 question7 click" data-many="3" data-row= "2">
+      <div class="col-lg-2 col-3 answer answer4" data-order="1">
+        <p class="reltop">Сергели</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer5" data-order="2">
+        <p class="reltop">Учтепа</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer6" data-order="3">
+        <p class="reltop">Хамза</p>
+      </div>
+    </div>
+    <div class="row justify-content-center question6 question8 click"  data-many="3" data-row= "3">
+      <div class="col-lg-2 col-3 answer answer7" data-order="1"><p class="reltop relright">Шайхантаур</p> </div>
+      <div class="col-lg-2 col-3 answer answer8" data-order="2"><p class="reltop">Юнусабад</p>
+        <i class="fa fa-arrows-v" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-2 col-3 answer answer9" data-order="3">
+        <p class="reltop">Чиланзар</p>
+      </div>
+      </div>
+    <div class="row justify-content-center question6 question9 click"  data-many="2" data-row="4">
+      <div class="col-lg-2 col-3 answer answer10"  data-order="1">
+        <p>Сабир-Рахимов</p>
+      </div>
+      <div class="col-lg-2 col-3 answer answer11"  data-order="2">
+        <p class="reltop relrightyak">Яккасарай</p>
+      </div>
+      </div>
+      </div>
+      
+  <div class="footer">
+    <p>&copy; Copyright</p>
+  </div>
+<p class="main-back"><span class="main-back-text">Назад</span><i class="fa fa-undo" aria-hidden="true"></i>
+  </p>
+  <script src="js/jquery_3.2.1.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/script.js"></script>
+  <script src="js/bootstrap.js"></script>
+ 
+</body>
+
+</html>
